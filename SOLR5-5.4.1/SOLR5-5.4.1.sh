76abd4410bf43746b6ea0b0fe3ff5ccb8c4c7491
Federico Mendez <fmendez@gbif.org>
2017-06-06 14:08:31 +0200

    Solr5 should be downloaded from apache archives, is not available in mirror sites anymore

--- SOLR5-5.4.1/SOLR5-5.4.1.sh
+++ SOLR5-5.4.1/SOLR5-5.4.1.sh
@@ -1,7 +1,7 @@
 set -e
 SOLR_VERSION=5.4.1
 PARCEL_ROOT_DIR=SOLR5-$SOLR_VERSION
-wget http://mirrors.dotsrc.org/apache/lucene/solr/$SOLR_VERSION/solr-$SOLR_VERSION.tgz
+wget http://archive.apache.org/dist/lucene/solr/$SOLR_VERSION/solr-$SOLR_VERSION.tgz
 mkdir $PARCEL_ROOT_DIR
 tar xzf solr-$SOLR_VERSION.tgz -C $PARCEL_ROOT_DIR --strip 1
 mv  $PARCEL_ROOT_DIR/bin/solr.in.sh $PARCEL_ROOT_DIR/bin/solr.in.sh_base
