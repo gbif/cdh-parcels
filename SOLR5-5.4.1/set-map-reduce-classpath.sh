16e4bc7ab6f1555dd6a6a1f5ed0006f4f5d762e1
Federico Mendez <fmendez@gbif.org>
2017-08-09 14:08:27 +0200

    using specific jar locations and removing all hadoop files in HADOOP_CLASSPATH

--- SOLR5-5.4.1/set-map-reduce-classpath.sh
+++ SOLR5-5.4.1/set-map-reduce-classpath.sh
@@ -34,6 +34,14 @@ dir5=`absPath "$solr_distrib/contrib/morphlines-cell/lib"`
 dir6=`absPath "$solr_distrib/contrib/extraction/lib"`
 dir7=`absPath "$solr_distrib/server/solr-webapp/webapp/WEB-INF/lib"`
 
+libC1=`ls $dir1/*.jar | awk '{print "file://"$1":"}' | tr -d ' \n' | sed 's/.$//'`
+libC2=`ls $dir2/*.jar | awk '{print "file://"$1":"}' | tr -d ' \n' | sed 's/.$//' | sed 's/\:[^\:]*\(log4j\|slf4j\)[^\:]*//g'`
+libC3=`ls $dir3/*.jar | awk '{print "file://"$1":"}' | tr -d ' \n' | sed 's/.$//' | sed 's/\:[^\:]*\(hadoop\)[^\:]*//g'`
+libC4=`ls $dir4/*.jar | awk '{print "file://"$1":"}' | tr -d ' \n' | sed 's/.$//'`
+libC5=`ls $dir5/*.jar | awk '{print "file://"$1":"}' | tr -d ' \n' | sed 's/.$//'`
+libC6=`ls $dir6/*.jar | awk '{print "file://"$1":"}' | tr -d ' \n' | sed 's/.$//'`
+libC7=`ls $dir7/*.jar | awk '{print "file://"$1":"}' | tr -d ' \n' | sed 's/.$//' | sed 's/\:[^\:]*\(hadoop\)[^\:]*//g'`
+
 # Setup -libjar
 
 lib1=`ls $dir1/*.jar | awk '{print "file://"$1","}' | tr -d ' \n' | sed 's/.$//'`
@@ -44,9 +52,8 @@ lib5=`ls $dir5/*.jar | awk '{print "file://"$1","}' | tr -d ' \n' | sed 's/.$//'
 lib6=`ls $dir6/*.jar | awk '{print "file://"$1","}' | tr -d ' \n' | sed 's/.$//'`
 lib7=`ls $dir7/*.jar | awk '{print "file://"$1","}' | tr -d ' \n' | sed 's/.$//' | sed 's/\,[^\,]*\(hadoop\)[^\,]*//g'`
 
-export HADOOP_CLASSPATH="$dir1/*:$dir2/*:$dir3/*:$dir4/*:$dir5/*:$dir6/*:$dir7/*"
+export HADOOP_CLASSPATH="$libC1:$libC2:$libC3:$libC4:$libC5:$libC6:$libC7"
 export HADOOP_LIBJAR="$lib1,$lib2,$lib3,$lib4,$lib5,$lib6,$lib7"
 
 #echo $HADOOP_CLASSPATH
 #echo $HADOOP_LIBJAR
-
